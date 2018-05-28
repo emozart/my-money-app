b6085c4db7605d27b85f0d40ec299f73561b32d7
Elton Mozart <elton.mozart@gmail.com>
2018-05-28 16:48:40 -0300

    Configuração das rotas

---
+++ frontend/src/billingCycle/billingCycle.jsx
@@ -0,0 +1,7 @@
+import React from 'react'
+
+export default pŕops => (
+    <div>
+        <h1>Ciclos de Pagamento</h1>
+    </div>
+)
---
+++ frontend/src/dashboard/dashboard.jsx
@@ -0,0 +1,7 @@
+import React from 'react'
+
+export default pŕops => (
+    <div>
+        <h1>Dashboard</h1>
+    </div>
+)
